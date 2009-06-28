06eb35ce9ea638043f10aa3238896ec722d03701
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2009-06-28 02:05:34 +0000

    Adding Samples Gallery

---
+++ trunk/samples-gallery/startServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Copyright 2008 Google Inc. All Rights Reserved.
+# Author: pamelafox@google.com (Pamela Fox)
+../../google_appengine/dev_appserver.py . --address="0.0.0.0" --debug
---
+++ trunk/samples-gallery/uploadApp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+../../google_appengine/appcfg.py --email=pamela.fox@gmail.com update .
