79209983612ea19c8102aa813005efff10656a8a
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2010-04-30 14:16:59 +0000

    Adding People Finder project, de-IOized

---
+++ samples/extensions/robots/python/people-finder/interests.py
@@ -0,0 +1,23 @@
+# Break this out into it's own file to be 
+# pulled in in the generation of the seach
+# gadget.
+INTERESTS = [
+ #  (id, label), use an id of '--' for section headers
+    ('--',           'Languages'),
+    ('cpp',          'C++'),
+    ('java',         'Java'),
+    ('javascript',   'JavaScript'),
+    ('python',       'Python'),
+    ('--',           'Products'),
+    ('android',      'Android'),
+    ('appengine',    'App Engine'),
+    ('chrome',       'Chrome'),
+    ('enterprise',   'Enterprise'),
+    ('geo',          'Geo'),
+    ('apis',         'Google APIs'),
+    ('gwt',          'Google Web Toolkit'),
+    ('social',       'Social Web'),
+    ('wave',         'Wave'),
+    ]
+
+
