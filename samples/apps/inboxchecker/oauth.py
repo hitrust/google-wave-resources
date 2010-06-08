e7afabe6e4e58e5fb1f38f74e625ac26d47b0079
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2010-06-08 14:03:22 +0000

    Halfway through new OAuth

--- samples/apps/inboxchecker/oauth.py
+++ samples/apps/inboxchecker/oauth.py
@@ -2,20 +2,57 @@ import wsgiref.handlers
 import logging
 import pickle
 import uuid
+import httplib
+import urllib
+
+from waveapi import oauth
 
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 
-import gdata
-import gdata.service
-import atom.http
+REQUEST_TOKEN_URL = 'https://www.google.com/accounts/OAuthGetRequestToken'
+ACCESS_TOKEN_URL = 'https://www.google.com/accounts/OAuthGetAccessToken'
+AUTHORIZATION_URL = 'https://www.google.com/accounts/OAuthAuthorizeToken'
+
+# example client using httplib with headers
+class SimpleOAuthClient(oauth.OAuthClient):
+
+    def __init__(self, server, port=httplib.HTTP_PORT, request_token_url='', access_token_url='', authorization_url=''):
+        self.server = server
+        self.port = port
+        self.request_token_url = request_token_url
+        self.access_token_url = access_token_url
+        self.authorization_url = authorization_url
+        self.connection = httplib.HTTPSConnection('www.google.com')
+
+    def fetch_request_token(self, oauth_request):
+        # via headers
+        # -> OAuthToken
+        self.connection.request(oauth_request.http_method, self.request_token_url+'?'+urllib.urlencode(oauth_request.parameters)) 
+        response = self.connection.getresponse()
+        return oauth.OAuthToken.from_string(response.read())
+
+    def fetch_access_token(self, oauth_request):
+        # via headers
+        # -> OAuthToken
+        self.connection.request(oauth_request.http_method, self.access_token_url, headers=oauth_request.to_header()) 
+        response = self.connection.getresponse()
+        return oauth.OAuthToken.from_string(response.read())
+
+    def authorize_token(self, oauth_request):
+        # via url
+        # -> typically just some okay response
+        self.connection.request(oauth_request.http_method, oauth_request.to_url()) 
+        response = self.connection.getresponse()
+        return response.getheader('location')#.read()
 
 class OAuthRequestToken(db.Model):
   """Stores OAuth request token."""
 
   token_key = db.StringProperty(required=True)
-  token_secret = db.StringProperty(required=True)
+  token_secret = db.StringProperty()
   created = db.DateTimeProperty(auto_now_add=True)
+  token_ser = db.TextProperty()
 
 class OAuthAccessToken(db.Model):
   """Stores OAuth request token."""
@@ -28,56 +65,54 @@ class OAuthAccessToken(db.Model):
 
 
 class OAuthHandler(webapp.RequestHandler):
+  SIGNATURE_METHOD = oauth.OAuthSignatureMethod.HMAC_SHA1
+  SCOPE = 'http://wave.googleusercontent.com/api/rpc'
+  COOKIE = 'wavesid'
   CONSUMER_KEY = 'anonymous'
   CONSUMER_SECRET = 'anonymous'
-  #CONSUMER_KEY = 'wavesandbox.com'
-  #CONSUMER_SECRET = 'veBIDzuTYiJZsAeA/LCFGxgo'
-  SIGNATURE_METHOD = gdata.auth.OAuthSignatureMethod.HMAC_SHA1
-  SCOPE = 'http://wave.googleusercontent.com/api/rpc'
-  OAUTH_INPUT_PARAMS = gdata.auth.OAuthInputParams(
-                                    SIGNATURE_METHOD,
-                                    CONSUMER_KEY,
-                                    CONSUMER_SECRET)
-  COOKIE = 'wavesid'
 
   def __init__(self):
-    self._service = gdata.service.GDataService()
-    self._service.SetOAuthInputParameters(
-                                    OAuthHandler.SIGNATURE_METHOD,
-                                    OAuthHandler.CONSUMER_KEY,
-                                    OAuthHandler.CONSUMER_SECRET)
+    self._client = SimpleOAuthClient("localhost", 80, REQUEST_TOKEN_URL, ACCESS_TOKEN_URL, AUTHORIZATION_URL)
+    self._consumer = oauth.OAuthConsumer(OAuthHandler.CONSUMER_KEY, OAuthHandler.CONSUMER_SECRET)
+
 
 class LoginHandler(OAuthHandler):
 
   def get(self):
+    # Step 1: Fetch Request Token, specifying desired scope
     callback_url = "%s/oauth/verify" % self.request.host_url
-    request_token = self._service.FetchOAuthRequestToken(scopes=OAuthHandler.SCOPE)
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(self._consumer, callback=callback_url, http_url=self._client.request_token_url, parameters = {'scope':OAuthHandler.SCOPE)
+    oauth_request.sign_request(OAuthHandler.SIGNATURE_METHOD, self._consumer, None)
+    request_token = self._client.fetch_request_token(oauth_request)
     logging.info('Request Token fetched: %s' % request_token)
-    self._service.SetOAuthToken(request_token)
+
     # When using HMAC, persist the token secret in order to re-create an
     # OAuthToken object coming back from the approval page.
-    db_token = OAuthRequestToken(token_key = request_token.key,
-        token_secret=request_token.secret)
+    db_token = OAuthRequestToken(token_key = request_token.key, token_ser = request_token.to_string())
     db_token.put()
 
-    auth_url = self._service.GenerateOAuthAuthorizationURL(callback_url=callback_url)
+    # Step 2: Generate authorization URL
+    oauth_request = oauth.OAuthRequest.from_token_and_callback(token=request_token, http_url=self._client.authorization_url)
+    auth_url = client.authorize_token(oauth_request)
     logging.info('Authorization URL: %s' % auth_url)
+    # Redirect to URL for authorization
     return self.redirect(auth_url)
 
 class VerifyHandler(OAuthHandler):
 
   def get(self):
-    oauth_token = gdata.auth.OAuthTokenFromUrl(self.request.uri)
-    if oauth_token:
-      # Find request token saved by put() method.
-      db_token = OAuthRequestToken.all().filter(
-        'token_key =', oauth_token.key).fetch(1)[0]
-      oauth_token.secret = db_token.token_secret
-      self._service.SetOAuthToken(oauth_token)
+    logging.info(self.request)
+    verifier = self.request.get('oauth_verifier')
+    token_key = self.request.get('oauth_token')
+    # Find request token saved by put() method.
+    db_token = OAuthRequestToken.all().filter(
+      'token_key =', token_key).fetch(1)[0]
+    token = oauth.OAuthToken.from_string(tokenstr)
 
     # 3.) Exchange the authorized request token for an access token
-    access_token = self._service.UpgradeToOAuthAccessToken()
-    access_token.oauth_input_params = OAuthHandler.OAUTH_INPUT_PARAMS
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(self._consumer, token=token, verifier=verifier, http_url=self._client.access_token_url)
+    oauth_request.sign_request(OAuthHandler.SIGNATURE_METHOD, self._consumer, token)
+    access_token = self._client.fetch_access_token(oauth_request)
 
     session_id = str(uuid.uuid1())
     db_token = OAuthAccessToken(session=session_id, token_key = access_token.key,
