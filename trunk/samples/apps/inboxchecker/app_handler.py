b1064dcd3e21bae3ccd46b045f81e3f93244a78f
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2010-06-25 08:16:01 +0000

    Making fetch wave work better, use blipconverter

--- trunk/samples/apps/inboxchecker/app_handler.py
+++ trunk/samples/apps/inboxchecker/app_handler.py
@@ -1,5 +1,6 @@
 import wsgiref.handlers
 import logging
+import string
 from time import gmtime, strftime
 
 from google.appengine.ext import webapp
@@ -7,6 +8,34 @@ from django.utils import simplejson
 
 from waveapi import search
 import oauth_handler
+import blipconverter
+
+def results_from_json(json):
+    if isinstance(json, basestring):
+      json = simplejson.loads(json)
+    search_results = search.Results(json['data']['searchResults'])
+    return search_results
+
+def html_for_results(results):
+    html = '%s, (%s)<br>' % (results.query, results.num_results)
+    for digest in results.digests:
+      url = 'http://wave.google.com/wave/#restored:wave:%s' % digest.wave_id
+      url = '/app/fetchwave?wave_id=%s' % digest.wave_id.replace('+', '%252B')
+      html += '<a href="%s"><b>%s</b></a><br>%s' % (url, digest.title, digest.snippet)
+      date = strftime("%a, %d %b %Y %H:%M:%S", gmtime(digest.last_modified/1000))
+      html += '%s/%s:%s' % (str(digest.unread_count), str(digest.blip_count),
+                            date)
+      participants = [p for p in digest.participants]
+      html += '(%s)' % ','.join(participants)
+      html += '<br><br>'
+    return html
+
+def html_for_wavelet(wavelet):
+  html_list = []
+  for blip_id in wavelet.blips:
+    blip = wavelet.blips.get(blip_id)
+    html_list.append(blipconverter.ToHTML(blip))
+  return string.join(html_list, '<br><br>')
 
 class InboxHandler(oauth_handler.DataRequestHandler):
 
@@ -20,55 +49,16 @@ class InboxHandler(oauth_handler.DataRequestHandler):
     html = html_for_results(search_results)
     return self.response.out.write(html)
 
-def results_from_json(json):
-    if isinstance(json, basestring):
-      json = simplejson.loads(json)
-    search_results = search.Results(json['data']['searchResults'])
-    return search_results
-
-def html_for_results(results):
-    html = '%s, (%s)<br>' % (results.query, results.num_results)
-    for digest in results.digests:
-      url = 'http://wave.google.com/wave/#restored:wave:%s' % digest.wave_id
-      html += '<a href="%s"><b>%s</b></a><br>%s' % (url, digest.title, digest.snippet)
-      date = strftime("%a, %d %b %Y %H:%M:%S", gmtime(digest.last_modified/1000))
-      html += '%s/%s:%s' % (str(digest.unread_count), str(digest.blip_count),
-                            date)
-      participants = [p for p in digest.participants]
-      html += '(%s)' % ','.join(participants)
-      html += '<br><br>'
-    return html
-
 class FetchWaveHandler(oauth_handler.DataRequestHandler):
 
   def get(self):
     if not self.get_token():
       return
-    wave_id = 'googlewave.com!w+_ZvqGPcnH'
-    # digest wave
-    #wave_id = 'googlewave.com!w+QH8ZW5LQt'
-    wavelet_id = 'googlewave.com!conv+root'
-    wave_id = 'googlewave.com!w+0xNca70qA'
-    wave_id = 'googlewave.com!w+TNVx0ka5A'
-    wave_id = 'googlewave.com!w+vlQQ9rZkC26'
-    #wave_id = 'googlewave.com!w%252BQIK8uMFQA'
-    wave_id = 'googlewave.com!w+G7ex3qzpA'
-    notify_op = "{'id':'0', 'method':'wave.robot.notifyCapabilitiesHash', 'params': {'protocolVersion': '0.21'}}"
-    fetch_op = "{'id':'op1', 'method':'wave.robot.fetchWave','params':{'waveId':'%s', 'waveletId': '%s'}}" % (wave_id, wavelet_id)
-    data = "[%s, %s]" % (notify_op, fetch_op)
-    response = self.perform_operation(data)
-    return self.response.out.write(response);
-
-class FolderHandler(oauth_handler.DataRequestHandler):
-
-  def get(self):
-    if not self.get_token():
-      return
-    wave_id = 'googlewave.com!w+_ZvqGPcnH'
-    data = "{'id':'op1', 'method':'wave.robot.folderAction','params':{'modifyHow': 'markAsRead', 'waveId':'%s'}}" % (wave_id)
-    data = "{'id':'op1', 'method':'wave.robot.folderAction','params':{'waveId':'%s'}}" % (wave_id)
-    response = self.perform_operation(data)
-    return self.response.out.write(response);
+    DIGEST_WAVE_ID = 'googlewave.com!w+SgYrEnoLE'
+    wave_id = self.request.get('wave_id', DIGEST_WAVE_ID)
+    wave_id = wave_id.replace('%2B', '+')
+    wavelet = self._service.fetch_wavelet(wave_id, 'googlewave.com!conv+root')
+    return self.response.out.write(html_for_wavelet(wavelet));
 
 class MainHandler(webapp.RequestHandler):
 
@@ -80,7 +70,6 @@ def main():
                                         ('/', MainHandler),
                                         ('/app/inbox', InboxHandler),
                                         ('/app/fetchwave', FetchWaveHandler),
-                                        ('/app/folder', FolderHandler)
                                         ],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
