8909f95971a539f43582ac206c9fb4e59a3c261c
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2010-07-09 08:18:22 +0000

    Adding Formy sample

---
+++ trunk/samples/extensions/robots/python/formy/forminfo.py
@@ -0,0 +1,23 @@
+title = 'Title'
+
+intro = """
+Some useful information here.
+"""
+
+fields = [
+  {'name': 'name',
+   'label': 'Name'},
+  {'name': 'email',
+   'label': 'Email'},
+  {'name': 'summary',
+   'label': 'Summary',
+   'extra': '100 chars'},
+  {'name': 'description',
+   'label': 'Description',
+   'type': 'textarea',
+   'extra': '100 words'}
+]
+
+tags = ['form']
+
+participants = ['casey.johnson1990@googlewave.com']
