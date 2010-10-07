2a3bffed98abcac6cde3981ef61dd0dbcd3859a7
pamela.fox <pamela.fox@99de6746-39ed-11de-b310-9106b1f7c239>
2010-10-07 05:23:02 +0000

    New submitty version (not sure if it actually works)

---
+++ trunk/samples/extensions/robots/python/submitty/config.py
@@ -0,0 +1,4 @@
+APPROVERS = ['pamela.fox@googlewave.com','amandapsurya@googlewave.com','joe.gregorio@googlewave.com']
+REVIEWERS = ['pamela.fox@googlewave.com','amandapsurya@googlewave.com','joe.gregorio@googlewave.com','google-wave-extensions-review@googlegroups.com']
+GALLERYGROUP = 'google-wave-extension-gallery-all@googlegroups.com'
+GALLERYMOD = 'doctorwave.gallery@googlewave.com'
--- trunk/samples/extensions/robots/python/submitty/message.py
+++ trunk/samples/extensions/robots/python/submitty/message.py
@@ -1,62 +1,80 @@
-prelim = """
+# Strings used by robot when accidentally added
+ACC_TITLE = 'Hi there!'
+ACC_MESSAGE = """I am a robot that helps put extensions in the gallery. I hope you are enjoying the extensions! Please post in the help group if you have any questions about them.\n
+http://www.google.com/support/forum/p/wave/label?lid=1fe2aa0c757c9191&hl=en
+"""
+
+# Strings used in actual submission wave
+TITLE = 'Extension Submission'
+
+INTRO = """
 Congratulations on completing your extension!
 
 Welcome to the extension review process. We have put together a series of questions that will help us learn more about your extension and how users will use it inside Wave. When you are done responding, you can let us know by clicking the "Share with Reviewers" button above. If you haven't yet, we suggest reading through the <a href="http://code.google.com/apis/wave/extensions/designprinciples.html">design principles</a> and <a href="http://code.google.com/apis/gadgets/docs/publish.html#Hi_Volume">gadgets performance</a> docs.
 
-<b>First, some basic info about you:</b>
-
-<b>Name</b>:
-<b>Location</b>:
-
-<b>Now, some info about your extension:</b>
-
-<b>Name</b>:
-<b>Sample Wave</b>:
-<i>Please host on Google Wave Preview. If it is not a public wave, please add google-wave-extensions-review@googlegroups.com to the wave so that we can access it.</i>
-<b>Installer</b>:
-
-
+First, some basic info about you:
 """
 
-prelim_robotq = "Does your extension include a robot?"
+DEV_FIELDS = [
+    {'name': 'dev_name', 'label': 'Developer/Company Name'},
+    {'name': 'location', 'label': 'Location'}
+    ]
 
-prelim_gadgetq = "Does your extension include a gadget?"
+MIDDLE = """
+Now, some info about your extension:
+"""
 
-robot = """
+EXT_FIELDS = [
+    {'name': 'samplewave',
+     'label': 'Sample Wave',
+     'extra': 'Please host on Google Wave Preview. If it is not a public wave, please add google-wave-extensions-review@googlegroups.com to the wave so that we can access it.'},
+    {'name': 'installer_url',
+     'label': 'Installer URL'}
+    ]
+
+ROBOT_QS = """
 Great! Please answer the following questions about the robot:
 
 - For each event that the robot reacts to, how does it respond?
-- What does the robot do when first added to a Wave (if anything)?
-- Can/does this bot work in conjunction with any other robots?
 - Does the user have to know some special syntax for using your robot? (We recommend avoiding this when possible)
 """
 
-gadget = """
+GADGET_QS = """
 Great! Please answer the following questions about the gadget:
 
 - What data is shared in the state object? Please describe the key/value mapping.
 - How frequenty is the state updated?
-- Does the gadget use setModeCallback to provide different behavior in edit
-  versus view mode? If so, please describe.
+- Does the gadget use setModeCallback to provide different behavior in edit versus view mode? If so, please describe.
 - What does the user see in playback mode?
 - How does one user know what/where another user is editing?
 - Is the gadget constant sized? If not, please describe the gadget resizing.
 - Does the gadget provide instructions to users?
-- Does the gadget provide a way for users to give feedback?
+- What other resources (images/data) does your gadget bring in? For each resource, please note the server that the resource is on, and if you are using gadget caching or some other caching technique.
+
+Please also consider these tips:
+- To increase the virality of your extension, we recommend adding a link from the gadget to a public wave with an installer.
+- To get feedback on your extension, we suggest adding a link from the gadget to a feedback form, issue tracker, or a public discussion wave.
 """
 
 
-questions = """
-<b>Performance/Stability:</b>
-- If it is a gadget:
-What other resources does your gadget bring in? For each resource, please note the server that the resource is on, and if you are using gadget caching or some other caching technique.
+GAE_QS = """
+Okay, please answer these questions about your App Engine use:
 
-- If it is a robot:
-
-What additional web services does it contact? For each web service, please note the rate limits and expected load of that service.
-Have you enabled billing on your app engine account for your robot's app?
-Have you profiled your robot to make sure that your code is optimized in terms of CPU usage?
-If you are using a datastore on App Engine, please describe the reads and writes, and whether memcache is being used to cache hits.
-If storing static files on App Engine, please note the cache headers that you've used.
-If rendering dynamic pages on App Engine, please comment on the use of memcache for temporarily caching these renders.
+- What additional web services does it contact? For each web service, please note the rate limits and expected load of that service.
+- Have you enabled billing for your app? If not, please monitor the app to ensure it stays below the free quota limits.
+- If you are using a datastore on App Engine, please describe the reads and writes, and whether memcache is being used to cache hits.
+- If storing static files on App Engine, please note the cache headers that you've used.
+- If rendering dynamic pages on App Engine, please comment on the use of memcache for caching these renders.
 """
+
+EXT_QS = [
+    {'name': 'robotqs',
+     'label': 'Does your extension include a robot?',
+     'response': ROBOT_QS},
+    {'name': 'gadgetqs',
+     'label': 'Does your extension include a gadget?',
+     'response': GADGET_QS},
+    {'name': 'gaeqs',
+     'label': 'Does your extension use App Engine?',
+     'response': GAE_QS}
+    ]
