8878b4c2bdea075f8c3ac05e8fb86cca2d4be7b0
tirsen <tirsen@google.com>
2011-04-14 04:47:43 +0000

    More margin.

--- samples/extensions/gadgets/playtube/gadget.js
+++ samples/extensions/gadgets/playtube/gadget.js
@@ -727,7 +727,7 @@ function onAVStateChange(state) {
  */
 function resizeYouTubePlayer() {
   var viewportHeight = gadgets.window.getViewportDimensions().height;
-  var nonytHeight = DOM.PTT.height() + $('#player-header').height() + $('#player-control-container').height() + 30;
+  var nonytHeight = DOM.PTT.height() + $('#player-header').height() + $('#player-control-container').height() + 50;
   $('#myytplayer').height(viewportHeight - nonytHeight);
 }
 $(function() {
